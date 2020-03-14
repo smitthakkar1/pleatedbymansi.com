ed61b37cc519321ff5cc78bf506126883258feae
Smit Thakkar <smitthakkar@Smits-MacBook-Air.local>
2020-03-14 19:30:00 -0400

    style and add social media

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import instagram from './instagram.png';
+import youtube from './youtube.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="PleatedHeader">
+          <p className="Pleated">Pleated By Mansi</p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="Coming">
           Coming Soon ...
         </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/pleated_by_mansi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={instagram}
-            alt="instagram" 
-            className="Instagram" 
-          />
-        </a>
+        <div className="SocialContainer">
+          <a
+            className="App-link"
+            href="https://www.instagram.com/pleated_by_mansi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={instagram}
+              alt="instagram" 
+              className="Instagram" 
+            />
+          </a>
+          <a
+            className="App-link"
+            href="https://www.youtube.com/channel/UCI8VjcgjbR4Yt_-vwnrXw-Q"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={youtube}
+              alt="youtube" 
+              className="Instagram" 
+            />
+          </a>
+        </div>
       </header>
     </div>
   );
